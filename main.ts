4834662a4864a50f15ab949afa472c80e74e4526
Luis Santos <48485199+pajeeh@users.noreply.github.com>
2024-08-05 17:09:11 -0300

    Alterado para pin P1 receber ponto, e pin P2 receber traço

--- main.ts
+++ main.ts
@@ -2,20 +2,20 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         basic.showLeds(`
             . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
+            # # . . .
+            # # . . .
+            . . . . .
             . . . . .
             `)
         basic.pause(1000)
         basic.clearScreen()
     } else if (receivedNumber == 1) {
         basic.showLeds(`
+            . . . . .
             # # # # #
-            # . . . #
-            # . # . #
-            # . . . #
             # # # # #
+            . . . . .
+            . . . . .
             `)
         basic.pause(1000)
         basic.clearScreen()
